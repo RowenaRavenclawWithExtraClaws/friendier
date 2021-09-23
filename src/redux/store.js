925b4c3f26c361934f717201c389752f5143fefc
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-23 11:43:41 +0200

    add more to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import peopleReducer from "./peopleSlice";
+import personReducer from "./personSlice";
+import isFriendsModalOpenReducer from "./isFriendsModalOpenSlice";
 
 const store = configureStore({
   reducer: {
     people: peopleReducer,
+    person: personReducer,
+    isFriendModalOpen: isFriendsModalOpenReducer,
   },
 });
 
