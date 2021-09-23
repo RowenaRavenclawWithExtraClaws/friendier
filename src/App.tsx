4197ee5f422a282e6f2b8c4f4730bde5aeadd4ab
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-23 03:48:02 +0200

    use the appTitle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import "./App.css";
+import AppTitle from "./components/appTitle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <AppTitle />
       </header>
     </div>
   );
