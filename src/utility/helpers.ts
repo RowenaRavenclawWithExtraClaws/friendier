00fe7a8fa089a8753ff28aa8859ab634543de1ed
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-24 01:11:01 +0200

    add helper functions to find person by id and render notification banner

--- src/utility/helpers.ts
+++ src/utility/helpers.ts
@@ -1,6 +1,29 @@
+import toastr from "toastr";
 import { Person } from "./types";
 
 export const getFriends = (
   friendIndexes: Array<number>,
   people: Array<Person>
 ) => friendIndexes.map((indx) => people[indx]);
+
+export const findPersonIndxByEmail = (people: Array<Person>, email: string) => {
+  let personIndx = -1;
+
+  people.forEach((person, indx) => {
+    if (email === person.email) personIndx = indx;
+  });
+
+  return personIndx;
+};
+
+// display notification banner
+export const Notify = (msg: string, title: string, type: string) => {
+  toastr.options = {
+    positionClass: "toast-bottom-right",
+    newestOnTop: true,
+  };
+
+  if (type === "success") toastr.success(msg, title);
+  else if (type === "error") toastr.error(msg, title);
+  else toastr.warning(msg, title);
+};
