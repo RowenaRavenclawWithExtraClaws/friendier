0412f839f08ba2bed23107a4cb2aeae57a87345c
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-23 05:13:34 +0200

    implement the app design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import AppTitle from "./components/appTitle";
+import CustomButton from "./components/customButton";
+import { Col, Row } from "reactstrap";
+import CustomCard from "./components/customCard";
 
 function App() {
+  const buttons = [
+    { text: "Add person", styleClass: "btn-primary", handler: () => {} },
+    { text: "Make friends", styleClass: "btn-primary", handler: () => {} },
+    { text: "Find connections", styleClass: "btn-primary", handler: () => {} },
+  ];
+
   return (
     <div className="app">
-      <header className="app-header">
+      <div className="app-header">
         <AppTitle />
-      </header>
+        <Row className="button-row" style={{ marginTop: "2rem" }}>
+          {buttons.map((button, indx) => (
+            <Col key={indx} lg="4">
+              <CustomButton
+                text={button.text}
+                styleClass={button.styleClass}
+                handler={button.handler}
+              />
+            </Col>
+          ))}
+        </Row>
+        <CustomCard />
+      </div>
     </div>
   );
 }
