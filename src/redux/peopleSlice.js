2e78f80ff4e1f0da3f3eb5bae7819c64ef6c36c9
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-24 01:10:01 +0200

    add addFriend reducer

--- src/redux/peopleSlice.js
+++ src/redux/peopleSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findPersonIndxByEmail } from "../utility/helpers";
 
 const initialState = {
   value: {
@@ -39,10 +40,27 @@ export const peopleSlice = createSlice({
 
       state.value.people = tempPeople;
     },
+    connectPeople: (state, action) => {
+      const tempPeople = state.value.people;
+
+      const personIndx1 = findPersonIndxByEmail(
+        tempPeople,
+        action.payload.email1
+      );
+      const personIndx2 = findPersonIndxByEmail(
+        tempPeople,
+        action.payload.email2
+      );
+
+      tempPeople[personIndx1].friends.push(personIndx2);
+      tempPeople[personIndx2].friends.push(personIndx1);
+
+      state.value.people = tempPeople;
+    },
   },
 });
 
-export const { setPeople, addPerson } = peopleSlice.actions;
+export const { setPeople, addPerson, connectPeople } = peopleSlice.actions;
 
 export const selectPeople = (state) => state.people.value.people;
 
