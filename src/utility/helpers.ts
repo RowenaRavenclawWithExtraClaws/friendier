b20e6ecb1af466f553f1600978a53c889b7d2f48
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-24 05:04:41 +0200

    implement a function to compute the connection

--- src/utility/helpers.ts
+++ src/utility/helpers.ts
@@ -16,6 +16,40 @@ export const findPersonIndxByEmail = (people: Array<Person>, email: string) => {
   return personIndx;
 };
 
+export const findConnetions = (
+  people: Array<Person>,
+  personIndx1: number,
+  personIndx2: number
+) => {
+  let visited: Array<boolean> = new Array(people.length).fill(false);
+  let stack: Array<number> = [];
+  let connections: Array<number> = [];
+  let connection: Array<number> = [];
+  let personIndx: number = -1;
+
+  stack.push(personIndx1);
+
+  while (stack.length !== 0 && personIndx !== personIndx2) {
+    personIndx = stack.pop() as number;
+
+    if (!visited[personIndx]) {
+      connections.push(personIndx);
+      visited[personIndx] = true;
+      people[personIndx].friends.forEach((friend) => stack.push(friend));
+    }
+  }
+
+  const distIndx = connections.indexOf(personIndx2);
+  let i = distIndx;
+
+  while (connections[i] !== personIndx1 && i >= 0) {
+    connection.unshift(connections[i]);
+    i -= 1;
+  }
+
+  return [connections[i], ...connection];
+};
+
 // display notification banner
 export const Notify = (msg: string, title: string, type: string) => {
   toastr.options = {
